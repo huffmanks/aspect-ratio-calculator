48e918e0a83aa0c65cc57130b0987a0577f78c63
Kevin Huffman <huffmanks2@gmail.com>
2021-11-07 18:40:29 -0500

    updated form

--- index.js
+++ index.js
@@ -1,16 +1,107 @@
+// Form
 const calculator = document.querySelector('#calculator')
+const formControls = document.querySelectorAll('.form-control')
 const originalWidth = document.querySelector('#original-width')
 const originalHeight = document.querySelector('#original-height')
-const newWidth = document.querySelector('#new-width')
-const newHeight = document.querySelector('#new-height')
 
-calculator.addEventListener('keyup', () => {
-    console.log(newHeight.value)
-    if (newHeight.value) {
-        newWidth.value = (originalWidth.value / originalHeight.value) * newHeight.value
+// New Size
+const newSize = document.querySelector('#new-size')
+
+// Clear Button
+const clearBtn = document.querySelector('#clearBtn')
+
+// Output
+const aspectBox = document.querySelector('.aspect-box')
+const aspectNumbers = document.querySelector('.aspect-numbers')
+const newWidth = document.querySelector('.new-width')
+const newHeight = document.querySelector('.new-height')
+const aspectWidth = document.querySelector('.aspect-width')
+const aspectHeight = document.querySelector('.aspect-height')
+
+// Toast
+const errorToast = document.querySelector('#errorToast')
+const closeToast = document.querySelector('#closeToast')
+
+calculator.addEventListener('change', () => {
+    // Error message
+    formControls.forEach((formControl) => {
+        closeBtn.addEventListener('click', () => {
+            formControl.classList.remove('is-invalid')
+            formControl.value = ''
+        })
+        if (/\D/.test(formControl.value)) {
+            formControl.classList.add('is-invalid')
+            const toast = new bootstrap.Toast(errorToast)
+            toast.show()
+
+            setTimeout(() => {
+                formControl.classList.remove('is-invalid')
+                formControl.value = ''
+                toast.hide()
+            }, 5000)
+        }
+    })
+
+    // Selected radio
+    const selectedNewValue = document.querySelector('input[name="new-value"]:checked')
+
+    if (selectedNewValue.value === 'width') {
+        newSize.placeholder = 'Width'
+        newSize.nextElementSibling.innerText = 'Width'
     }
 
-    if (newWidth.value) {
-        newHeight.value = (originalHeight.value / originalWidth.value) * newWidth.value
+    if (selectedNewValue.value === 'height') {
+        newSize.placeholder = 'Height'
+        newSize.nextElementSibling.innerText = 'Height'
     }
+
+    // Calculate new size
+    if (originalWidth.value && originalHeight.value) {
+        // If new width is selected
+        if (selectedNewValue.value === 'width') {
+            let newOtherSize = Math.round((originalHeight.value / originalWidth.value) * newSize.value * 100) / 100
+
+            aspectBox.style.aspectRatio = newSize.value / newOtherSize
+
+            newWidth.innerText = newSize.value
+            newHeight.innerText = newOtherSize
+            aspectHeight.innerText = '1'
+
+            let aspectRat = newSize.value / newOtherSize
+            if (aspectRat === 1) {
+                aspectWidth.innerText = 1
+            } else {
+                aspectWidth.innerText = (newSize.value / newOtherSize).toFixed(6)
+            }
+        }
+
+        // If new height is selected
+        if (selectedNewValue.value === 'height') {
+            let newOtherSize = Math.round((originalWidth.value / originalHeight.value) * newSize.value * 100) / 100
+
+            aspectBox.style.aspectRatio = newOtherSize / newSize.value
+
+            newWidth.innerText = newOtherSize
+            newHeight.innerText = newSize.value
+            aspectWidth.innerText = '1'
+
+            let aspectRat = newSize.value / newOtherSize
+            if (aspectRat === 1) {
+                aspectHeight.innerText = 1
+            } else {
+                aspectHeight.innerText = (newSize.value / newOtherSize).toFixed(6)
+            }
+        }
+    }
+
+    // Output numbers
+    if (newSize.value) {
+        aspectNumbers.style.display = 'block'
+    }
+})
+
+// Clear form
+clearBtn.addEventListener('click', () => {
+    aspectNumbers.style.display = 'none'
+    calculator.reset()
 })
